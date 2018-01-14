f36f8dd1535c2f55b833b9968b3b2036c6d0e1df
JC009 <32849971+JC009@users.noreply.github.com>
2018-01-14 21:57:35 +0100

    menu
    dodane menu wraz z potwierdzeniem wyjścia

---
+++ Assets/_Scripts/SkryptMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class SkryptMenu : MonoBehaviour
+{
+    public Canvas menuWyjscia;
+    public Button przyciskRozpoczecia;
+    public Button przyciskWyjscia;
+    void Start()
+    {
+        menuWyjscia = menuWyjscia.GetComponent<Canvas>();
+        przyciskRozpoczecia = przyciskRozpoczecia.GetComponent<Button>();
+        przyciskWyjscia = przyciskWyjscia.GetComponent<Button>();
+        menuWyjscia.enabled = false;
+    }
+    public void PrzyciskWyjsciaAktywny()
+    {
+        menuWyjscia.enabled = true;
+        przyciskRozpoczecia.enabled = false;
+        przyciskWyjscia.enabled = false;
+    }
+    public void PrzyciskNieAktywny()
+    {
+        menuWyjscia.enabled = false;
+        przyciskRozpoczecia.enabled = true;
+        przyciskWyjscia.enabled = true;
+    }
+    public void PoziomStartowy()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+    public void ZakonczGre()
+    {
+        Application.Quit();
+    }
+}
