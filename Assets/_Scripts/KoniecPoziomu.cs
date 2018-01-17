e5b18aea26d6c712baa034b0f817ffd60e1ab160
JC009 <32849971+JC009@users.noreply.github.com>
2018-01-17 19:07:36 +0100

    nowa mapa

---
+++ Assets/_Scripts/KoniecPoziomu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KoniecPoziomu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void Kolizja (Collision2D kolider)
+    {
+        SceneManager.LoadScene("MenuGL", LoadSceneMode.Single);
+    }
+}
