c47a9d495481794dc481bcf02b913b4f4baf9d51
PawelAdam <32848855+PawelAdam@users.noreply.github.com>
2018-01-17 23:28:37 +0100

    Revert "Małe zmiany"
    This reverts commit a1347567d76173d513d64d2e02de675cd89d3f31.

--- Assets/_Scripts/SkryptZycia.cs
+++ Assets/_Scripts/SkryptZycia.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SkryptZycia : MonoBehaviour {
-    public GameObject zdrowie1, zdrowie2, zdrowie3, zdrowie4, zdrowie5, GameOver, Pauza;
+    public GameObject zdrowie1, zdrowie2, zdrowie3, zdrowie4, zdrowie5, GameOver;
     private int zycie;
 	// Use this for initialization
 	void Start () {
@@ -14,7 +14,7 @@ public class SkryptZycia : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetMouseButtonDown(0)&&!(Pauza.activeInHierarchy))
+        if (Input.GetMouseButtonDown(0))
         {
             switch (zycie)
             {
