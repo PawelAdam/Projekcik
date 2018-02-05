f3f95510f01a31367075b19e102266bd0f5e0e9d
Arkadiusz <arek01996@gmail.com>
2018-02-05 16:52:34 +0100

    Poprawiony weapon

--- Assets/_Scripts/Weapon.cs
+++ Assets/_Scripts/Weapon.cs
@@ -4,29 +4,53 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour {
 
-    private float currentAngle;
+    public float currentAngle;
+    private float direct;
     public float maxAngle;
     public float weaponSpeed;
-
+    public GameObject Player;
 	// Use this for initialization
 	void Start () {
         currentAngle = 0.0f;
-
+        direct = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Vector3 x_mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float x_player = Player.transform.position.x;
+
+        // Obracaj bronią
+        if(currentAngle == 0)
+        {
+            if(x_mouse.x < x_player)
+            {
+                direct = 0;
+                gameObject.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+            }else
+            {
+                direct = 180.0f;
+                gameObject.transform.rotation = Quaternion.Euler(new Vector3(0f, 180f, 0f));
+            }
+        }
+
+        
         if (Input.GetMouseButton(0))
         {
             if(currentAngle < maxAngle)
             {
                 currentAngle += weaponSpeed * Time.deltaTime;
-                gameObject.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, currentAngle));
+                gameObject.transform.rotation = Quaternion.Euler(new Vector3(0f, direct, currentAngle));
             }else
             {
                 currentAngle = 0.0f;
-                gameObject.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+                gameObject.transform.rotation = Quaternion.Euler(new Vector3(0f, direct, 0f));
             }
+        }
+        if (Input.GetMouseButtonUp(0) && currentAngle != 0.0f)
+        {
+            currentAngle = 0.0f;
+            gameObject.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
         }
     }
 }
