bc66f09bf0a5c183c6f7912f97cc79cc018baeb5
Arkadiusz <arek01996@gmail.com>
2018-02-05 17:11:20 +0100

    Efekt krwi przy zadaniu obrażenia
    Bardzo taki prosty ale jest xD

--- Assets/_Scripts/SkryptZycia.cs
+++ Assets/_Scripts/SkryptZycia.cs
@@ -9,22 +9,46 @@ public class SkryptZycia : MonoBehaviour {
     public int PlayerHP;
     private bool enemyCollision;
     public Text GameOver;
-
-	// Use this for initialization
-	void Start () {
+    public Material background;
+    public float takeHPTime; // IN seconds
+    private float currentTakeHPTime;
+    private bool takeHP;
+    private Color materialColor;
+    // Use this for initialization
+    void Start () {
         enemyCollision = false;
         GameOver.enabled = false;
+        currentTakeHPTime = 0;
+        takeHP = true;
+        materialColor = Color.white;
+        background.color = materialColor;
     }
 	
 	// Update is called once per frame
 	void Update () {
-
-        if (enemyCollision)
+        if(!takeHP)
         {
-            
+            if(currentTakeHPTime < takeHPTime)
+            {
+                
+                materialColor.g = 1 - ((currentTakeHPTime / takeHPTime));
+                materialColor.b = 1- ((currentTakeHPTime / takeHPTime));
+                currentTakeHPTime += Time.deltaTime;
+                background.color = materialColor;
+            }
+            else
+            {
+                background.color = Color.white;
+                currentTakeHPTime = 0;
+                takeHP = true;
+            }
+        }
+        if (enemyCollision && takeHP)
+        { 
             enemyCollision = false;
            if(PlayerHP > 0)
             {
+                takeHP = false;
                 PlayerHP--;
                 HP[PlayerHP].enabled = false;
             }
@@ -38,7 +62,7 @@ public class SkryptZycia : MonoBehaviour {
 	}
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Enemy")
+        if(collision.gameObject.tag == "Enemy" && takeHP)
         {
             enemyCollision = true;
         }
